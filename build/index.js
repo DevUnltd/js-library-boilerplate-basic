c80b4c9d8f28a1b2ac980fcadb99b598865223fe
Francisco Hodge <hello@franciscohodge.com>
2021-03-07 20:46:54 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   @hodgef/js-library-boilerplate-basic v1.5.4
+ *   @hodgef/js-library-boilerplate-basic v1.5.5
  *   https://github.com/hodgef/js-library-boilerplate-basic
  *
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef) and project contributors.
